9fb5d1da900cd3753e40fa3bb55288390f2a62ae
Rounak Sen <rounaksen225@gmail.com>
2023-07-04 13:57:37 -0700

    Adding initial changes for CaloChallenge

--- plotting.py
+++ plotting.py
@@ -1,7 +1,12 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib import figure
 import mplhep as hep
+from matplotlib.colors import LogNorm
+import gc
 
+# matplotlib.use("PDF")
 plt.switch_backend("agg")
 plt.rcParams.update({"font.size": 16})
 plt.style.use(hep.style.CMS)
@@ -13,8 +18,14 @@ plabels_dict = {
 }
 
 
-def plot_part_feats(
-    jet_type,
+LAYER_SPECS = [(3, 96), (12, 12), (12, 6)]
+
+# from guppy import hpy
+
+# h = hpy()
+
+
+def plot_hit_feats(
     real_jets,
     gen_jets,
     real_mask=None,
@@ -29,62 +40,233 @@ def plot_part_feats(
     show=False,
 ):
     """Plot particle feature histograms"""
-    if coords == "cartesian":
-        bin = np.arange(-500, 500, 10)
-        pbins = [bin, bin, bin]
-    elif coords == "polarrel":
-        if dataset == "jetnet":
-            if jet_type == "g" or jet_type == "q" or jet_type == "w" or jet_type == "z":
-                if num_particles == 100:
-                    pbins = [
-                        np.arange(-0.5, 0.5, 0.005),
-                        np.arange(-0.5, 0.5, 0.005),
-                        np.arange(0, 0.1, 0.001),
-                    ]
-                else:
-                    pbins = [
-                        np.linspace(-0.3, 0.3, 100),
-                        np.linspace(-0.3, 0.3, 100),
-                        np.linspace(0, 0.2, 100),
-                    ]
-                    ylims = [3e5, 3e5, 3e5]
-            elif jet_type == "t":
-                pbins = [
-                    np.linspace(-0.5, 0.5, 100),
-                    np.linspace(-0.5, 0.5, 100),
-                    np.linspace(0, 0.2, 100),
-                ]
-    elif coords == "polarrelabspt":
-        pbins = [np.arange(-0.5, 0.5, 0.01), np.arange(-0.5, 0.5, 0.01), np.arange(0, 400, 4)]
+    plabels = ["$\eta$", "$\phi$", "z", "E (GeV)"]
+    pbins = [
+        np.linspace(0, 1, 51),
+        np.linspace(0, 1, 51),
+        np.linspace(0, 1, 51),
+        np.linspace(0, 50000, 51),
+    ]
 
-    plabels = plabels_dict[coords]
+    # plabels = plabels_dict[coords]
 
     if real_mask is not None:
         parts_real = real_jets[real_mask]
         parts_gen = gen_jets[gen_mask]
+        
+        
+
     else:
         parts_real = real_jets.reshape(-1, real_jets.shape[2])
         parts_gen = gen_jets.reshape(-1, gen_jets.shape[2])
 
-    fig = plt.figure(figsize=(22, 8))
 
-    for i in range(3):
-        fig.add_subplot(1, 3, i + 1)
+    
+
+    fig = plt.figure(figsize=(16, 16))
+
+    for i in range(4):
+        fig.add_subplot(2, 2, i + 1)
         plt.ticklabel_format(axis="y", scilimits=(0, 0), useMathText=True)
         _ = plt.hist(parts_real[:, i], pbins[i], histtype="step", label="Real", color="red")
         _ = plt.hist(parts_gen[:, i], pbins[i], histtype="step", label="Generated", color="blue")
-        plt.xlabel("Particle " + plabels[i])
-        plt.ylabel("Number of Particles")
-        if const_ylim:
-            plt.ylim(0, ylims[i])
-        if losses is not None and "w1p" in losses:
-            plt.title(
-                f'$W_1$ = {losses["w1p"][-1][i]:.2e} ± {losses["w1p"][-1][i + len(losses["w1p"][-1]) // 2]:.2e}',
-                fontsize=12,
-            )
+        plt.xlabel("Hit " + plabels[i])
+        plt.ylabel("Number of Hits")
+        if i == 3:
+            plt.yscale("log")
         plt.legend(loc=1, prop={"size": 18})
 
     plt.tight_layout(pad=2.0)
+
+    # TODO pull from new updates
+    # test temporarily forbid saving plots
+    if figs_path is not None and name is not None:
+        plt.savefig(figs_path + name + ".pdf", bbox_inches="tight")
+
+
+    if show:
+        plt.show()
+    else:
+        plt.close()
+        # gc.collect()
+
+
+def plot_layerwise_hit_feats(
+    real_showers,
+    gen_showers,
+    real_mask=None,
+    gen_mask=None,
+    name=None,
+    figs_path=None,
+    num_particles=30,
+    show=False,
+):
+    real_hits = real_showers[real_mask]
+    gen_hits = gen_showers[gen_mask]
+   
+
+    # try to print out
+    real_layer_etas = []
+    real_layer_phis = []
+    real_layer_Es = []
+
+    gen_layer_etas = []
+    gen_layer_phis = []
+    gen_layer_Es = []
+
+    for i in range(3):
+        real_layer_hits = real_hits[
+            (real_hits[:, 2] >= i * 0.33) * (real_hits[:, 2] < (i + 1) * 0.33)
+        ] 
+        # >=
+        # TODO delete as type (int)
+        # real_layer_etas.append(((real_layer_hits[:, 0] * LAYER_SPECS[i][1]) - 0.5).astype(int))
+        real_layer_etas.append(((real_layer_hits[:, 0] * LAYER_SPECS[i][1]) - 0.5))
+        # real_layer_phis.append(((real_layer_hits[:, 1] * LAYER_SPECS[i][0]) - 0.5).astype(int))
+        real_layer_phis.append(((real_layer_hits[:, 1] * LAYER_SPECS[i][0]) - 0.5))
+        real_layer_Es.append(real_layer_hits[:, 3])
+
+        gen_layer_hits = gen_hits[(gen_hits[:, 2] >= i * 0.33) * (gen_hits[:, 2] < (i + 1) * 0.33)]
+        # gen_layer_etas.append(((gen_layer_hits[:, 0] * LAYER_SPECS[i][1]) - 0.5).astype(int))
+        gen_layer_etas.append(((gen_layer_hits[:, 0] * LAYER_SPECS[i][1]) - 0.5))
+        # gen_layer_phis.append(((gen_layer_hits[:, 1] * LAYER_SPECS[i][0]) - 0.5).astype(int))
+        gen_layer_phis.append(((gen_layer_hits[:, 1] * LAYER_SPECS[i][0]) - 0.5))
+        gen_layer_Es.append(gen_layer_hits[:, 3])
+
+    fig = plt.figure(figsize=(22, 22), constrained_layout=True)
+    fig.suptitle(" ")
+
+    subfigs = fig.subfigures(nrows=3, ncols=1)
+
+    for i, subfig in enumerate(subfigs):
+        subfig.suptitle(f"Layer {i + 1}")
+
+        # create 1x3 subplots per subfig
+        axs = subfig.subplots(nrows=1, ncols=3)
+        axs[0].hist(
+            real_layer_etas[i], bins=np.linspace(0, LAYER_SPECS[i][1] + 1, 101), histtype="step", label="Real"
+        )
+        axs[0].hist(
+            gen_layer_etas[i],
+            bins=np.linspace(0, LAYER_SPECS[i][1] + 1, 101),
+            histtype="step",
+            label="Generated",
+        )
+        axs[0].ticklabel_format(axis="y", scilimits=(0, 0), useMathText=True)
+        axs[0].set_xlabel(r"Hit $\eta$s")
+        axs[0].set_ylabel("Number of Hits")
+        axs[0].legend()
+
+        axs[1].hist(
+            real_layer_phis[i], bins=np.linspace(0, LAYER_SPECS[i][0] + 1, 101), histtype="step", label="Real"
+        )
+        axs[1].hist(
+            gen_layer_phis[i],
+            bins=np.linspace(0, LAYER_SPECS[i][0] + 1, 101),
+            histtype="step",
+            label="Generated",
+        )
+        axs[1].ticklabel_format(axis="y", scilimits=(0, 0), useMathText=True)
+        axs[1].set_xlabel(r"Hit $\varphi$s")
+        axs[1].set_ylabel("Number of Hits")
+        axs[1].legend()
+
+        bins = np.linspace(0, 50, 51) if i < 2 else np.linspace(0, 4, 51)
+        axs[2].hist(real_layer_Es[i] / 1000, bins=bins, histtype="step", label="Real")
+        axs[2].hist(gen_layer_Es[i] / 1000, bins=bins, histtype="step", label="Generated")
+        axs[2].set_xlabel("Hit Energies (GeV)")
+        axs[2].set_ylabel("Number of Hits")
+        axs[2].set_yscale("log")
+        axs[2].legend()
+
+    if figs_path is not None and name is not None:
+        print(f"saving fig at {figs_path} {name}")
+        plt.savefig(figs_path + name + ".pdf", bbox_inches="tight")
+
+
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
+
+def to_image(phi_size, eta_size, phis, etas, Es):
+    jet_image = np.zeros((phi_size, eta_size))
+
+    for eta, phi, pt in zip(phis, etas, Es):
+        if eta >= 0 and eta < eta_size and phi >= 0 and phi < phi_size:
+            jet_image[phi, eta] += pt
+
+    return jet_image
+
+
+def plot_shower_ims(
+    gen_showers,
+    name=None,
+    figs_path=None,
+    show=False,
+):
+    gen_ims = []
+
+    NUM_IMS = 1000
+
+    for j in range(3):
+        gen_layer_ims = []
+        for i in range(NUM_IMS):
+            gen_layer_hits = gen_showers[i][
+                (gen_showers[i][:, 2] > j * 0.33) * (gen_showers[i][:, 2] < (j + 1) * 0.33)
+            ]
+            gen_etas = ((gen_layer_hits[:, 0] * LAYER_SPECS[j][1]) - 0.5).astype(int)
+            gen_phis = ((gen_layer_hits[:, 1] * LAYER_SPECS[j][0]) - 0.5).astype(int)
+            gen_Es = gen_layer_hits[:, 3]
+            gen_layer_ims.append(
+                to_image(LAYER_SPECS[j][0], LAYER_SPECS[j][1], gen_etas, gen_phis, gen_Es)
+            )
+
+        gen_ims.append(gen_layer_ims)
+
+    vmins = [1e-2, 1e-2, 1e-4]
+    vmaxs = [1e5, 1e3, 10]
+
+    fig = plt.figure(figsize=(24, 32), constrained_layout=True)
+    fig.suptitle(" ")
+
+    subfigs = fig.subfigures(nrows=1, ncols=3)
+
+    for i, subfig in enumerate(subfigs):
+        subfig.suptitle(f"Layer {i + 1}")
+
+        axs = subfig.subplots(nrows=6, ncols=1)
+
+        for j in range(5):
+            pos = axs[j].imshow(
+                gen_ims[i][j],
+                aspect="auto",
+                cmap="binary",
+                # vmin=vmins[i],
+                # vmax=vmaxs[i],
+                norm=LogNorm(vmins[i], vmaxs[i]),
+                extent=(0, LAYER_SPECS[i][1], 0, LAYER_SPECS[i][0]),
+            )
+            axs[j].set_xlabel(r"$\eta$")
+            axs[j].set_ylabel(r"$\varphi$")
+            fig.colorbar(pos, ax=axs[j])
+
+        j = 5
+        axs[j].set_title("Average Image")
+        pos = axs[j].imshow(
+            np.mean(np.array(gen_ims[i]), axis=0),
+            aspect="auto",
+            cmap="binary",
+            # vmin=vmins[i],
+            # vmax=vmaxs[i],
+            norm=LogNorm(vmins[i], vmaxs[i]),
+            extent=(0, LAYER_SPECS[i][1], 0, LAYER_SPECS[i][0]),
+        )
+        axs[j].set_xlabel(r"$\eta$")
+        axs[j].set_ylabel(r"$\varphi$")
+        fig.colorbar(pos, ax=axs[j])
+
     if figs_path is not None and name is not None:
         plt.savefig(figs_path + name + ".pdf", bbox_inches="tight")
 
@@ -297,44 +479,44 @@ def plot_jet_feats(
         plt.close()
 
 
-# def plot_jet_mass_pt(realjf, genjf, dataset="jetnet", name=None, figs_path=None, show=False):
-#     if dataset == "jetnet":
-#         jlabels = ["Jet Relative Mass", "Jet Relative $p_T$"]
-#         binsm = np.linspace(0, 0.225, 101)
-#         binspt = np.linspace(0.5, 1.2, 101)
-#     elif dataset == "jets-lagan":
-#         jlabels = ["Jet Mass (GeV)", "Jet $p_T$ (GeV)"]
-#         binsm = np.linspace(40, 120, 51)
-#         binspt = np.linspace(220, 340, 51)
-#
-#     fig = plt.figure(figsize=(16, 8))
-#
-#     fig.add_subplot(1, 2, 1)
-#     plt.ticklabel_format(axis="y", scilimits=(0, 0), useMathText=True)
-#     # plt.ticklabel_format(axis='x', scilimits=(0, 0), useMathText=True)
-#     _ = plt.hist(realjf[:, 0], bins=binsm, histtype="step", label="Real", color="red")
-#     _ = plt.hist(genjf[:, 0], bins=binsm, histtype="step", label="Generated", color="blue")
-#     plt.xlabel(jlabels[0])
-#     plt.ylabel("Jets")
-#     plt.legend(loc=1, prop={"size": 18})
-#
-#     fig.add_subplot(1, 2, 2)
-#     plt.ticklabel_format(axis="y", scilimits=(0, 0), useMathText=True)
-#     plt.ticklabel_format(axis="x", scilimits=(0, 0), useMathText=True)
-#     _ = plt.hist(realjf[:, 1], bins=binspt, histtype="step", label="Real", color="red")
-#     _ = plt.hist(genjf[:, 1], bins=binspt, histtype="step", label="Generated", color="blue")
-#     plt.xlabel(jlabels[1])
-#     plt.ylabel("Jets")
-#     plt.legend(loc=1, prop={"size": 18})
-#
-#     plt.tight_layout(pad=2)
-#     if figs_path is not None and name is not None:
-#         plt.savefig(figs_path + name + ".pdf", bbox_inches="tight")
-#
-#     if show:
-#         plt.show()
-#     else:
-#         plt.close()
+def plot_jet_mass_pt(realjf, genjf, dataset="jetnet", name=None, figs_path=None, show=False):
+    if dataset == "jetnet":
+        jlabels = ["Jet Relative Mass", "Jet Relative $p_T$"]
+        binsm = np.linspace(0, 0.225, 101)
+        binspt = np.linspace(0.5, 1.2, 101)
+    elif dataset == "jets-lagan":
+        jlabels = ["Jet Mass (GeV)", "Jet $p_T$ (GeV)"]
+        binsm = np.linspace(40, 120, 51)
+        binspt = np.linspace(220, 340, 51)
+
+    fig = plt.figure(figsize=(16, 8))
+
+    fig.add_subplot(1, 2, 1)
+    plt.ticklabel_format(axis="y", scilimits=(0, 0), useMathText=True)
+    # plt.ticklabel_format(axis='x', scilimits=(0, 0), useMathText=True)
+    _ = plt.hist(realjf[:, 0], bins=binsm, histtype="step", label="Real", color="red")
+    _ = plt.hist(genjf[:, 0], bins=binsm, histtype="step", label="Generated", color="blue")
+    plt.xlabel(jlabels[0])
+    plt.ylabel("Jets")
+    plt.legend(loc=1, prop={"size": 18})
+
+    fig.add_subplot(1, 2, 2)
+    plt.ticklabel_format(axis="y", scilimits=(0, 0), useMathText=True)
+    plt.ticklabel_format(axis="x", scilimits=(0, 0), useMathText=True)
+    _ = plt.hist(realjf[:, 1], bins=binspt, histtype="step", label="Real", color="red")
+    _ = plt.hist(genjf[:, 1], bins=binspt, histtype="step", label="Generated", color="blue")
+    plt.xlabel(jlabels[1])
+    plt.ylabel("Jets")
+    plt.legend(loc=1, prop={"size": 18})
+
+    plt.tight_layout(pad=2)
+    if figs_path is not None and name is not None:
+        plt.savefig(figs_path + name + ".pdf", bbox_inches="tight")
+
+    if show:
+        plt.show()
+    else:
+        plt.close()
 
 
 def plot_losses(losses, loss="lg", name=None, losses_path=None, show=False):
@@ -418,19 +600,14 @@ def plot_eval(
         fig.add_subplot(3, 3, 5)
         plt.plot(x, means)
         plt.xlabel("Epoch")
-        plt.ylabel(r"FPD")
+        plt.ylabel(r"$\overline{\mathrm{FGD}}_{\infty}$")
         plt.yscale("log")
         plt.ylim(top=10)
 
-    if "kpd" in losses:
-        means = np.array(losses["kpd"])[:, 0]
-        stds = np.array(losses["kpd"])[:, 1]
-
-        # plot mean + 1 sigma values to avoid -ve values in log plot
         fig.add_subplot(3, 3, 6)
         plt.plot(x, means + stds)
         plt.xlabel("Epoch")
-        plt.ylabel(r"KP$D^{+\sigma}$")
+        plt.ylabel(r"$\overline{\mathrm{FGD}}_{\infty}^{+\sigma}$")
         plt.yscale("log")
         plt.ylim(top=10)
 
